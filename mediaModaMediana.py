3053d0cc5ee4cb1a2b525ecc7f0e04b3bcfc20cb
AngelPuent3 <99633191+AngelPuent3@users.noreply.github.com>
2022-05-22 00:26:36 -0500

    Update mediaModaMediana.py

--- mediaModaMediana.py
+++ mediaModaMediana.py
@@ -1,34 +1,44 @@
+
 class mediModaMediana():
     
     def __init__(self):
-        file = open("numeros.txt", "r")
-        self.go = file.readlines()
-        self.lista = [int(i) for i in self.go]
-        
+        self.valoresEncuesta = []
 
+
+
+    def obtenerListaArchivo(self):
+        archivo = open("numeros.txt", "r")
+        lineaEncuestaString = archivo.readlines()[0]
+        arrayEncuestaString = lineaEncuestaString.split()
+        for x in arrayEncuestaString:
+            self.valoresEncuesta.append(int(x))
+ 
 
     def media(self):
-        print(sum(self.lista) / len(self.lista))
+        print(sum(self.valoresEncuesta) / len(self.valoresEncuesta))
     
     def moda(self):
-        self.lista.sort()
+        self.valoresEncuesta.sort()
         frecuencia = []
-        for i in range(len(self.lista)):
-            if self.lista[i] not in frecuencia:
-                frecuencia.append(self.lista[i])
+        for i in range(len(self.valoresEncuesta)):
+            if self.valoresEncuesta[i] not in frecuencia:
+                frecuencia.append(self.valoresEncuesta[i])
         frecuencia.sort()
         print(frecuencia[-1])
 
-    def mediana(self, lista):
-        lista.sort()
-        if len(lista) % 2 == 0:
-            return (lista[int(len(lista)/2)] + lista[int(len(lista)/2)-1]) / 2
+    def mediana(self):
+        self.valoresEncuesta.sort()
+        if len(self.valoresEncuesta) % 2 == 0:
+            return (self.valoresEncuesta[int(len(self.valoresEncuesta)/2)] + valores[int(len(self.valoresEncuesta)/2)-1]) / 2
         else:
-            return lista[int(len(lista)/2)]
+            return self.valoresEncuesta[int(len(self.valoresEncuesta)/2)]
 
 
 c=mediModaMediana()
 
-c.date()
+c.media()
+c.moda()
+c.mediana()
+
 
 
